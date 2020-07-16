3ebca2899850f2e7aca90191df298f04373f1d57
VisRex <ds67828@yandex.ru>
2020-07-16 19:42:02 +0300

    Main
    Laravel 5.3

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+header("Location: http://laraveltest.ru/laravel/public/");
+?>
---
+++ laravel/resources/views/welcome.blade.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            * {box-sizing: border-box;}
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+            .card {
+                right: 20px;
+                display: inline-block;
+                box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+                max-width: 300px;
+                margin: auto;
+                text-align: center;
+                font-family: arial;
+            }
+
+            .price {
+                color: grey;
+                font-size: 22px;
+            }
+
+            .card button {
+                border: none;
+                outline: 0;
+                padding: 12px;
+                color: white;
+                background-color: #000;
+                text-align: center;
+                cursor: pointer;
+                width: 100%;
+                font-size: 18px;
+            }
+
+            .card button:hover {
+                opacity: 0.7;
+            }
+            .product-item:hover .button {background: #fc5a5a;}
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Конструктор техники
+                </div>
+
+                <div class="card">
+                    <img src="jeans3.jpg" alt="Джинсы из денима" style="width:100%">
+                    <h1>Скроенные джинсы</h1>
+                    <p class="price">$19.99</p>
+                    <p>Некоторый текст о джинсах..</p>
+                    <p><button>Добавить карточку</button></p>
+                </div>
+                <div class="card">
+                    <img src="jeans3.jpg" alt="Джинсы из денима" style="width:100%">
+                    <h1>Скроенные джинсы</h1>
+                    <p class="price">$19.99</p>
+                    <p>Некоторый текст о джинсах..</p>
+                    <p><button>Добавить карточку</button></p>
+                </div>
+
+        </div>
+    </body>
+</html>
